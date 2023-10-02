5a9ae2936e62a1078a6e1802ae18dc2561a5e64c
ibtasam116 <ibtasam116@gmail.com>
2023-10-02 16:10:07 +0500

    Carsousel Lib Chnage

--- app/(sections)/client/client.js
+++ app/(sections)/client/client.js
@@ -2,8 +2,6 @@
 
 import Image from 'next/image'
 
-import Carousel from 'react-elastic-carousel'
-import { useRef } from "react";
 
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
 
@@ -12,12 +10,75 @@ import profile from '../../../public/images/profile-images/Profile.png'
 import profile1 from '../../../public/images/profile-images/Profile1.png'
 import ClientData from '@/app/(components)/clientData/clientData';
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 const Client = () => {
-
-    const carouselRef = useRef(null);
-
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
     return (
         <>
+
+            <Carousel
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                infinite={true}
+                // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                // deviceType={this.props.deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+            >
+                <div>
+
+                    <ClientData
+                        heading="What do they think"
+                        profile={profile}
+                        name="Samantha Sench"
+                        edu="Student at University"
+                        description="“ Hi, it’s Samantha. After using it made me a lot of benefits, starting with convenience of setting a deadline of tasks and schedule after that the app is very simple using. “" />
+
+
+                </div>
+                <div>
+                    <ClientData
+                        heading="What do they think"
+                        profile={profile}
+                        name="Samanthaqwqw Sench"
+                        edu="Student at University"
+                        description="“ Hi, it’s Samantha. After using it made me a lot of benefits, starting with convenience of setting a deadline of tasks and schedule after that the app is very simple using. “" />
+
+                </div>
+
+            </Carousel>;
+
+
+
+
+
             <section className="xl:max-w-[1440px] mx-auto w-full">
 
                 <div className="h-full md:my-28">
@@ -30,24 +91,15 @@ const Client = () => {
 
                         <div className='px-3 sm:px-10 lg:px-20 md:py-20'>
 
-                            <Carousel pagination={false} showArrows={false} ref={carouselRef}>
+
+                            <ClientData
+                                heading="What do they think"
+                                profile={profile}
+                                name="Samantha Sench"
+                                edu="Student at University"
+                                description="“ Hi, it’s Samantha. After using it made me a lot of benefits, starting with convenience of setting a deadline of tasks and schedule after that the app is very simple using. “" />
 
 
-                                <ClientData
-                                    heading="What do they think"
-                                    profile={profile}
-                                    name="Samantha Sench"
-                                    edu="Student at University"
-                                    description="“ Hi, it’s Samantha. After using it made me a lot of benefits, starting with convenience of setting a deadline of tasks and schedule after that the app is very simple using. “" />
-
-                                <ClientData
-                                    heading="What do they think"
-                                    profile={profile1}
-                                    name="Muhammad IbTaSaM"
-                                    edu="Student at University"
-                                    description="“ Hi, it’s Muhammad IbTaSaM. Computer Scientist: Fast learner, goal-oriented and focusing on my problem-solving skills. Love to build projects to improve my skillset. “" />
-
-                            </Carousel>
 
                             <div className='mt-2 md:mt-8 text-[#2D76F9] text-center md:text-start'>
 
@@ -70,6 +122,14 @@ const Client = () => {
                 </div>
 
             </section>
+
+            {/* <ClientData
+                                    heading="What do they think"
+                                    profile={profile1}
+                                    name="Muhammad IbTaSaM"
+                                    edu="Student at University"
+                                    description="“ Hi, it’s Muhammad IbTaSaM. Computer Scientist: Fast learner, goal-oriented and focusing on my problem-solving skills. Love to build projects to improve my skillset. “" /> */}
+
         </>
     )
 }
