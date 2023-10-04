7bfe496f6c45bd187f3d537306c62f32d760ac53
ibtasam116 <ibtasam116@gmail.com>
2023-10-04 23:20:37 +0500

    data

--- app/(sections)/client/clientSlider.js
+++ app/(sections)/client/clientSlider.js
@@ -1,7 +1,7 @@
 "use client";
 import profile from "@/public/images/profile-images/Profile.png";
 import profile1 from "@/public/images/profile-images/Profile1.png";
-import ClientData from "@/app/(components)/clientData/ClientData";
+import ClientData from '../../(components)/clientData/clientData'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
