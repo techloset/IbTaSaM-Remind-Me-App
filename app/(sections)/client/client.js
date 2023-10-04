537d3c0b10568e735cba9a90df5b769feea1f4c2
ibtasam116 <ibtasam116@gmail.com>
2023-10-04 23:28:49 +0500

    Slider

--- app/(sections)/client/client.js
+++ app/(sections)/client/client.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import reviewsImage from "@/public/images/reviews.png";
-import clientSlider from "./clientSlider";
+import ClientSlider from "@/app/(components)/slider/clientSlider";
 const Client = () => {
   return (
     <section className="max-w-[100%]  mx-auto w-full">
@@ -15,7 +15,7 @@ const Client = () => {
             />
           </div>
           <div className="w-72 sm:w-96 md:w-full mx-auto px-3 sm:px-10 lg:px-20 md:py-20">
-            <clientSlider />
+            <ClientSlider />
           </div>
         </div>
       </div>
